5b340453e4b801b0c0bc298f191fe2c3aa96a457
Dame <patrickvito2012@gmail.com>
2024-11-02 13:27:44 +0100

    change;

--- src/components/wallets/wallets.js
+++ src/components/wallets/wallets.js
@@ -4,20 +4,20 @@ import ethLogo from "../../img/eth.png";
 import usdtLogo from "../../img/usdt.png";
 // import bnbLogo from "../../img/bnb.png";
 // import dogeLogo from "../../img/doge.png";
-// import tronLogo from "../../img/tron.png";
+import tronLogo from "../../img/tron.png";
 // import shibaLogo from "../../img/shiba.png";
 // import btccLogo from "../../img/btcc.png";
 
 export const wallets = [
   {
     coin: "Bitcoin",
-    address: "bc1q60fyad2pfvjra2lhweuhmesqwk7e9cfvagvx89",
+    address: "bc1qr9h0wum9xaqcxa3un4xxmrfu4m8dxvk9y68mnq",
     addressName: "BTC Address",
     icon: BtcLogo,
   },
   {
     coin: "ETH ",
-    address: "0x2c953a15A7a1AD4A28880cB15f37cC425006A779",
+    address: "0x0313c101FF62D86a8Aea219Cd2F20a37f959D43e",
     addressName: "ETH Address:",
     icon: ethLogo,
   },
@@ -29,8 +29,14 @@ export const wallets = [
   // },
   {
     coin: "USDT ERC20",
-    address: "0x2c953a15A7a1AD4A28880cB15f37cC425006A779",
+    address: "0x0313c101FF62D86a8Aea219Cd2F20a37f959D43e",
     addressName: "USDT ERC20 Address",
+    icon: usdtLogo,
+  },
+  {
+    coin: "USDT TRC20",
+    address: "TZCvrKmPhqNE7CtMkZCCpgxT5XHztqchnh",
+    addressName: "USDT TRC20 Address",
     icon: usdtLogo,
   },
   // {
@@ -45,12 +51,12 @@ export const wallets = [
   //   addressName: "Dogecoin Address",
   //   icon: dogeLogo,
   // },
-  // {
-  //   coin: "TRON ",
-  //   address: "TVdAzHK24oAa92FcGReEexRa59v4KCquwP",
-  //   addressName: "TRON Address",
-  //   icon: tronLogo,
-  // },
+  {
+    coin: "TRON ",
+    address: "TDMu47oU3JmtcGQK5Sw9d1xRgkGMwS9tCU",
+    addressName: "TRON Address",
+    icon: tronLogo,
+  },
   // {
   //   coin: "SHIBA",
   //   address: "0xef8Ea6d059dDD47AaD4F7A5F190352688600d554",
