ff56320fc480dd359d364babc260f1e5ab29cfc6
Dame <patrickvito2012@gmail.com>
2024-11-06 12:51:30 +0100

    added

--- src/components/Start/Start.jsx
+++ src/components/Start/Start.jsx
@@ -8,7 +8,10 @@ const Start = () => {
       <div className="start__grid">
         <div className="start__text">
           <h1 className="text-info fw-bold">Ready To Start Your Mining</h1>
-          <p>Just create an account on our site and start your first mining.</p>
+          <p>
+            Create an account on our website with copy trading to help maximze
+            profit
+          </p>
         </div>
         <div className="start__btn">
           <Link to="register" className="btn btn-info text-sec">
